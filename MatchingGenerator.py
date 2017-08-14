0dab084ef6cc8be340628027a8c68f510ba94235
Jonathan Baker <jmbaker94@users.noreply.github.com>
2017-08-13 23:56:25 -0500

    Create MatchingGenerator.py

---
+++ MatchingGenerator.py
@@ -0,0 +1,19 @@
+from Graph import Graph, Vertex
+
+
+def sort_edges_by_degree(g):
+    """
+        Given a graph g, returns a list of edges in g, sorted by minimum sum of the degrees of
+        the end points
+    """
+    for edge in g.edge_list:
+
+
+
+def find_maximum_matchings(g):
+    pass
+
+
+def main():
+  pass
+
